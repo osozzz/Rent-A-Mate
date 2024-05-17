9926edd335a9a25faa9e54e9180bc5fdb334377c
Alejandro Osorno <osozsz.zk@gmail.com>
2024-05-16 23:55:52 -0500

    Fix properties detail

--- Publication/views.py
+++ Publication/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from Authentication.models import Data
-from django.http import HttpResponseRedirect, HttpResponseNotFound
 from Renters.models import Acommodation
 from .models import Posts, Reviews
 
@@ -16,16 +15,15 @@ def create_post(request, acommodation_id):
         post.save()
         return redirect('publi-and-review', post_id=post.pk)
     return render(request, 'create_post.html')
- 
+
 def property_details_view(request, post_id):
-    post = get_object_or_404(Posts, pk=post_id)
+    post = get_object_or_404(Posts, id=post_id)
     data = get_object_or_404(Data, user=request.user)
     is_saved = post in data.saved_posts.all()
-    
     if request.method == 'POST':
         rating = request.POST.get('rating')
         comments = request.POST.get('comments')
-        if rating and comments: 
+        if rating and comments:
             review = Reviews.objects.create(
                 publisher=request.user,
                 post=post,
@@ -42,5 +40,5 @@ def property_details_view(request, post_id):
                 data.saved_posts.remove(post)
             return redirect('publi-and-review', post_id=post_id)
         else:
-            return render(request, 'property_details.html', {'post': post_id}, {'message': 'Por favor, proporciona tanto la calificación como los comentarios.'})
-    return render(request, 'property_details.html', {'post': post}, {'is_saved': is_saved})
+            return render(request, 'property_details.html', {'post': post_id, 'message': 'Por favor, proporciona tanto la calificación como los comentarios.'})
+    return render(request, 'property_details.html', {'post': post, 'is_saved': is_saved})
